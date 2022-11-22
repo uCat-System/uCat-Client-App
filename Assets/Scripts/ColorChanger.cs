40b7c4ce12584211523e4c8ba014e8a75f125f41
unknown <s.hosovsky@gmail.com>
2022-11-22 22:07:10 +0000

    Testing voice commands - changing shape colors

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+   private void SetColor(Transform transform, Color color)
+   {
+       transform.GetComponent<Renderer>().material.color = color;
+   }
+
+   public void UpdateColor(string[] values)
+   {
+       var colorString = values[0];
+       var shapeString = values[1];
+
+       if (ColorUtility.TryParseHtmlString(colorString, out var color))
+       {
+           if (!string.IsNullOrEmpty(shapeString))
+           {
+               var shape = transform.Find(shapeString);
+               if (shape) SetColor(shape, color);
+           }
+           else
+           {
+               for (int i = 0; i < transform.childCount; i++)
+               {
+                   SetColor(transform.GetChild(i), color);
+               }
+           }
+       }
+   }
+}
