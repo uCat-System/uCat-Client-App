d811452c9fd0cd48f0d459751ccc4f80dbad0a5e
luketurnerdev <luke@lukedturner.com>
2023-07-02 14:55:11 +1000

    add other script lists

--- Assets/Scripts/UI/Dialogue/DialogueScriptData.cs
+++ Assets/Scripts/UI/Dialogue/DialogueScriptData.cs
@@ -5,9 +5,24 @@ using System;
 [CreateAssetMenu(fileName = "DialogueScriptData", menuName = "ScriptableObjects/Dialogue Script Data")]
 public class DialogueScriptData : ScriptableObject
 {
-    public List<string> introScriptDialogueOptions = new List<string>();
 
+    // Scripts & Animations
+    public List<string> introScriptDialogueOptions = new List<string>();
     public List<AnimationDriver.CatAnimations> introScriptDialogueAnimations = new List<AnimationDriver.CatAnimations>();
+
+    public List<string> level1ScriptDialogueOptions = new List<string>();
+    public List<AnimationDriver.CatAnimations> level1ScriptDialogueAnimations = new List<AnimationDriver.CatAnimations>();
+
+    public List<string> level2ScriptDialogueOptions = new List<string>();
+    public List<AnimationDriver.CatAnimations> level2ScriptDialogueAnimations = new List<AnimationDriver.CatAnimations>();
+
+    public List<string> level3ScriptDialogueOptions = new List<string>();
+
+    public List<AnimationDriver.CatAnimations> level3ScriptDialogueAnimations = new List<AnimationDriver.CatAnimations>();
+
+
+
+
 
     public int timeBetweenLinesInSeconds = 0;
 
