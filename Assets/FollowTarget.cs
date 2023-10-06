4e6b79cbce1242927ac2cee8472556a5481ba3e9
luketurnerdev <luke@lukedturner.com>
2023-10-06 23:52:54 +1100

    add ability to stop and start camera pan

--- Assets/FollowTarget.cs
+++ Assets/FollowTarget.cs
@@ -5,9 +5,16 @@ public class FollowTarget : MonoBehaviour
     public Transform target;  // Reference to the target GameObject (set in the Unity Inspector)
     public float speed = 5.0f;  // Speed at which the follower moves
 
+    public bool moving;
+
     private void Update()
     {
-        if (target != null)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            moving = !moving;
+        }
+
+        if (target != null && moving)
         {
             // Calculate the direction from the follower to the target
             Vector3 direction = target.position - transform.position;
