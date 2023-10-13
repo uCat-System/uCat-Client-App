4695eb393c3fddb8f9ad138dee92f0f04c90817f
luketurnerdev <luke@lukedturner.com>
2023-10-14 09:27:55 +1100

    fix persist object script

--- Assets/PersistObject.cs
+++ Assets/PersistObject.cs
@@ -11,25 +11,11 @@ public class PersistObject : MonoBehaviour
             // This is the first instance, so make it persist through scene changes.
             DontDestroyOnLoad(this.gameObject);
             instance = this;
-            PersistChildren();
         }
         else if (instance != this)
         {
             // This is a duplicate, so destroy it.
             Destroy(this.gameObject);
-        }
-    }
-
-    private void PersistChildren()
-    {
-        foreach (Transform child in transform)
-        {
-            PersistObject childPersistScript = child.GetComponent<PersistObject>();
-            if (childPersistScript != null)
-            {
-                // Call Awake recursively on children with the PersistObject script.
-                childPersistScript.Awake();
-            }
         }
     }
 }
